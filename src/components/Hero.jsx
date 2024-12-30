c3c6d9ec5bb7b98eb12be524e7f9a1d7f7d28269
Domagoj Vukosavljević <domagojv2005@gmail.com>
2024-12-30 18:43:40 +0100

    fix: overflow on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import video from '../images/Hero.mp4'
 function Hero() {
     return (
         <section id="main">
-            <video src={video} autoPlay muted loop></video>
+            <video src={video} autoPlay loop muted playsinline></video>
             <div className='overlay-title'>
                 <h1 className='mb-1'>Sve za vaš <br />nezaboravan <br />event</h1>
                 <p className='overlay-text'>Oprema, produkcija i ulaznice na jednom mjestu</p>
