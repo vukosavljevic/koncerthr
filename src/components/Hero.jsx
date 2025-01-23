e145f8d53b9b6ee959a23a40e4f4e23663c71c3d
Domagoj Vukosavljević <domagojv2005@gmail.com>
2025-01-23 18:51:59 +0100

    myb works

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,17 @@ import video from '../images/Hero.mp4'
 function Hero() {
     return (
         <section id="main">
-            <video src={video}     
-            autoPlay
-            muted 
-            loop 
-            playsinline></video>
+        <video
+  src={video}
+  autoPlay
+  muted
+  loop
+  playsInline
+  preload="auto"
+>
+  <source src={video} type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
             <div className="video-overlay"></div>
             <div className='overlay-title'>
                 <h1 className='mb-1'>Sve za vaš <br />nezaboravan <br />event</h1>
