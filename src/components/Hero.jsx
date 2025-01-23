42c2e903a8748b3ee4a5a4252062ddda23e80997
Domagoj Vukosavljević <domagojv2005@gmail.com>
2025-01-23 19:12:30 +0100

    tryout message 100

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,9 @@ import video from '../images/Hero.mp4'
 function Hero() {
     return (
         <section id="main">
-        <video src={video} />
+        <video autoPlay muted loop playsinline width="250">
+  <source src={video} type="video/mp4" />
+</video>
          <div className="video-overlay"></div>
             <div className='overlay-title'>
                 <h1 className='mb-1'>Sve za vaš <br />nezaboravan <br />event</h1>
