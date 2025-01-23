51aa255ab93757b4edcfc38c922de561e00131e3
Domagoj Vukosavljević <domagojv2005@gmail.com>
2025-01-23 19:32:44 +0100

    add old code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,26 @@
 import video from '../images/Hero.mp4'
+import { useEffect, useState } from 'react';
+import image from '../images/dummy.png';
 
+function useDeviceType() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const userAgent = navigator.userAgent.toLowerCase();
+        const isMobileDevice = /android|iphone|ipad|ipod|blackberry|windows phone|opera mini|iemobile/i.test(userAgent);
+        setIsMobile(isMobileDevice);
+    }, []);
+
+    return isMobile;
+}
 function Hero() {
+    const isMobile = useDeviceType();
+
     return (
         <section id="main">
-        <video autoPlay muted loop playsInline width="250">
+        {isMobile ?<img src={image} alt="" style={{height: '100vh', width: '100vw'}}/> : <video autoPlay muted loop playsInline width="250">
   <source src={video} type="video/mp4" />
-</video>
+</video>}
          <div className="video-overlay"></div>
             <div className='overlay-title'>
                 <h1 className='mb-1'>Sve za vaš <br />nezaboravan <br />event</h1>
