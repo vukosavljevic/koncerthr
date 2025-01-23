bbbc1aa6c66b709e86f4d2c8c53c44865212f6d0
Domagoj Vukosavljević <domagojv2005@gmail.com>
2025-01-23 19:03:38 +0100

    dasda

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,8 @@ import video from '../images/Hero.mp4'
 function Hero() {
     return (
         <section id="main">
-        <video  
-  src={video}
-  autoPlay
-      playsInline
-    >
-  <source src={video} type="video/mp4" />
-  Your browser does not support the video tag.
-</video>
-            <div className="video-overlay"></div>
+        <video src={video} />
+         <div className="video-overlay"></div>
             <div className='overlay-title'>
                 <h1 className='mb-1'>Sve za vaš <br />nezaboravan <br />event</h1>
                 <p className='overlay-text'>Oprema, produkcija i ulaznice na jednom mjestu</p>
