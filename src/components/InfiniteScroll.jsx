bb37b8ad35ebfde1e57a4a89469c77d5cc33b625
Domagoj Vukosavljević <domagojv2005@gmail.com>
2025-01-27 20:54:40 +0100

    fix: target blank links

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -13,61 +13,61 @@ const ScrollingMenu = () => {
     return (
       <div className="logos" id="logo">
         <div className="logos-slide">
-          <a href="https://eu.taf.cz/">
-          <img src={taf} alt={`Logo ${1}`} key={1} target="_blank"/>
+          <a href="https://eu.taf.cz/" target="blank">
+          <img src={taf} alt={`Logo ${1}`} key={1}/>
           </a>
-          <a href="https://www.rcf.it/en/home">
-          <img src={rcf} alt={`Logo ${2}`} key={2} target="_blank"/>
+          <a href="https://www.rcf.it/en/home" target="blank">
+          <img src={rcf} alt={`Logo ${2}`} key={2}/>
           </a>
-          <a href="https://www.shure.com/en-EU">
-          <img src={shure} alt={`Logo ${3}`} key={3} target="_blank"/>
+          <a href="https://www.shure.com/en-EU" target="blank">
+          <img src={shure} alt={`Logo ${3}`} key={3}/>
           </a>
-          <a href="https://www.robe.cz/">
-          <img src={robe} alt={`Logo ${5}`} key={5} target="_blank"/>
+          <a href="https://www.robe.cz/" target="blank">
+          <img src={robe} alt={`Logo ${5}`} key={5}/>
           </a>
-          <a href="https://www.fos-lighting.eu/">
-          <img src={fos} alt={`Logo ${6}`} key={6} target="_blank"/>
+          <a href="https://www.fos-lighting.eu/" target="blank">
+          <img src={fos} alt={`Logo ${6}`} key={6}/>
           </a>
-          <a href="https://www.kv2audio.com/">
-          <img src={kv} alt={`Logo ${7}`} key={7} target="_blank"/>
+          <a href="https://www.kv2audio.com/" target="blank">
+          <img src={kv} alt={`Logo ${7}`} key={7}/>
           </a>
-          <a href="hhttps://www.midasconsoles.com/">
-          <img src={midas} alt={`Logo ${8}`} key={8} target="_blank"/>
+          <a href="hhttps://www.midasconsoles.com/" target="blank">
+          <img src={midas} alt={`Logo ${8}`} key={8}/>
           </a>
-          <a href="https://www.shure.com/en-EU">
-          <img src={shure} alt={`Logo ${3}`} key={3} target="_blank"/>
+          <a href="https://www.shure.com/en-EU" target="blank">
+          <img src={shure} alt={`Logo ${3}`} key={3}/>
           </a>
-          <a href="https://www.robe.cz/">
-          <img src={robe} alt={`Logo ${5}`} key={5} target="_blank"/>
+          <a href="https://www.robe.cz/" target="blank">
+          <img src={robe} alt={`Logo ${5}`} key={5}/>
           </a>
         </div>
         <div className="logos-slide">
-          <a href="https://eu.taf.cz/">
-          <img src={taf} alt={`Logo ${1}`} key={1} target="_blank"/>
+          <a href="https://eu.taf.cz/" target="blank">
+          <img src={taf} alt={`Logo ${1}`} key={1}/>
           </a>
-          <a href="https://www.rcf.it/en/home">
-          <img src={rcf} alt={`Logo ${2}`} key={2} target="_blank"/>
+          <a href="https://www.rcf.it/en/home" target="blank">
+          <img src={rcf} alt={`Logo ${2}`} key={2}/>
           </a>
-          <a href="https://www.shure.com/en-EU">
-          <img src={shure} alt={`Logo ${3}`} key={3} target="_blank"/>
+          <a href="https://www.shure.com/en-EU" target="blank">
+          <img src={shure} alt={`Logo ${3}`} key={3}/>
           </a>
-          <a href="https://www.robe.cz/">
-          <img src={robe} alt={`Logo ${5}`} key={5} target="_blank"/>
+          <a href="https://www.robe.cz/" target="blank">
+          <img src={robe} alt={`Logo ${5}`} key={5}/>
           </a>
-          <a href="https://www.fos-lighting.eu/">
-          <img src={fos} alt={`Logo ${6}`} key={6} target="_blank"/>
+          <a href="https://www.fos-lighting.eu/" target="blank">
+          <img src={fos} alt={`Logo ${6}`} key={6}/>
           </a>
-          <a href="https://www.kv2audio.com/">
-          <img src={kv} alt={`Logo ${7}`} key={7} target="_blank" />
+          <a href="https://www.kv2audio.com/" target="blank">
+          <img src={kv} alt={`Logo ${7}`} key={7} />
           </a>
-          <a href="hhttps://www.midasconsoles.com/">
-          <img src={midas} alt={`Logo ${8}`} key={8} target="_blank"/>
+          <a href="hhttps://www.midasconsoles.com/" target="blank">
+          <img src={midas} alt={`Logo ${8}`} key={8}/>
           </a>
-          <a href="https://www.shure.com/en-EU">
-          <img src={shure} alt={`Logo ${3}`} key={3} target="_blank"/>
+          <a href="https://www.shure.com/en-EU" target="blank">
+          <img src={shure} alt={`Logo ${3}`} key={3}/>
           </a>
-          <a href="https://www.robe.cz/">
-          <img src={robe} alt={`Logo ${5}`} key={5} target="_blank"/>
+          <a href="https://www.robe.cz/" target="blank">
+          <img src={robe} alt={`Logo ${5}`} key={5}/>
           </a>
         </div>
       </div>
